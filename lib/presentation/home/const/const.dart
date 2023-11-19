e6aa15595382033e1bef26ea16d507c6b49bd26d
Naufal As <naufal202@gmail.com>
2023-11-20 01:48:24 +0530

    updated the home page ui

--- lib/presentation/home/const/const.dart
+++ lib/presentation/home/const/const.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 const divider = Divider();
-const sizedbox = SizedBox(
+const sizedboxwidth = SizedBox(
   width: 25,
 );
-const backgroundcolor = Color.fromARGB(255, 24, 108, 104);
+const sizedboxheight = SizedBox(
+  height: 20,
+);
+const backgroundcolor = Color(0xFF18485F);
+List carouseslider = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe3WYVA8q3daiHXAh_87Mw4NaP2bd1R5OIGw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvl5Tr5wx2LsIhmXN-z7AmsTkCA18oFPkXEw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfG1MBPsMR-8kJmyRy_o37j6NcgW1Kfg9A2w&usqp=CAU",
+  "https://assets.tatacliq.com/medias/sys_master/images/31434974035998.jpg"
+];
--- lib/presentation/home/widget/widget.dart
+++ lib/presentation/home/widget/widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:voguevilla/presentation/home/const/const.dart';
@@ -23,11 +22,13 @@ class SearchBarField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       child: SizedBox(
-        height: 50,
+        height: 45,
         child: TextFormField(
           decoration: InputDecoration(
+              labelStyle: const TextStyle(fontWeight: FontWeight.w400),
+              labelText: "Search on Voguevilla",
               prefixIcon: const Icon(
                 Icons.search,
                 color: backgroundcolor,
@@ -42,3 +43,95 @@ class SearchBarField extends StatelessWidget {
     );
   }
 }
+
+class CatagoryCircleAvatar extends StatelessWidget {
+  final String itemname;
+  final AssetImage image;
+  const CatagoryCircleAvatar(
+      {super.key, required this.itemname, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Color(0xFF123D56),
+              backgroundImage: image,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          itemname,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+}
+
+class ContainerImage extends StatelessWidget {
+  final String image;
+  const ContainerImage({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                    image,
+                  ),
+                  fit: BoxFit.fill),
+              borderRadius: BorderRadius.circular(10))),
+    );
+  }
+}
+
+class UnMissableDeals extends StatelessWidget {
+  const UnMissableDeals({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        shrinkWrap: true,
+        itemCount: 4,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 5,
+          crossAxisCount: 2,
+        ),
+        itemBuilder: ((context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        unmissableDealsimage[index],
+                      )),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.red),
+              width: 50,
+            ),
+          );
+        }));
+  }
+}
+
+List<String> unmissableDealsimage = [
+  "assets/mobileoffers.jpeg",
+  "assets/laptop.jpeg",
+  "assets/skincare.jpeg",
+  "assets/fragrances.jpeg"
+];
